340a49c04a23d53e82d821486aaa91f4dc689b51
iynere <iynere@users.noreply.github.com>
2019-03-14 13:39:43 -0400

    rename orb

--- publish-alpha.sh
+++ publish-alpha.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 circleci config pack src > orb.yml
-circleci orb publish orb.yml circleci/gcp-bin-authz@dev:alpha
+circleci orb publish orb.yml circleci/gcp-binary-authorization@dev:alpha
 rm -rf orb.yml
